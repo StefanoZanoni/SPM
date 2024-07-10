e9d5cc6a12d4055f9885fcc3e50fa183b0c68c90
StefanoZanoni <stefano.zanoni00@gmail.com>
2024-07-10 18:46:54 +0200

    initial commit

---
+++ CMakeFiles/SPM.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/stefano/programming/CLionProjects/SPM/main.cpp" "CMakeFiles/SPM.dir/main.cpp.o" "gcc" "CMakeFiles/SPM.dir/main.cpp.o.d"
+  "/home/stefano/programming/CLionProjects/SPM/src/sequential/sequential.cpp" "CMakeFiles/SPM.dir/src/sequential/sequential.cpp.o" "gcc" "CMakeFiles/SPM.dir/src/sequential/sequential.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
