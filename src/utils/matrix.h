c925f09d3f714aa8dfd85bc26b1d3601c74d9ba6
StefanoZanoni <stefano.zanoni00@gmail.com>
2024-07-12 12:23:36 +0200

    minor improvements

--- src/utils/matrix.h
+++ src/utils/matrix.h
@@ -12,12 +12,14 @@
 class UTMatrix {
 
 public:
-    explicit UTMatrix(unsigned int size) : size(size),
-    data(static_cast<double*>(_mm_malloc(size * (size + 1) / 2 * sizeof(double), 32))) {
+    explicit UTMatrix(const size_t size) :
+    size(size),
+    data(static_cast<double*>(_mm_malloc(size * (size + 1) / 2 * sizeof(double), 32)))
+    {
         if (!data) {
             throw std::runtime_error("Memory allocation failed");
         }
-        for (unsigned int i = 0; i < size; ++i) {
+        for (size_t i = 0; i < size; ++i) {
             data[index(i, i)] = double (i + 1) / size;
         }
     }
@@ -27,26 +29,30 @@ public:
     }
 
     void setUpperDiagonals() {
-        for (unsigned int k = 1; k < size; ++k) {
-            for (unsigned int i = 0; i < size - k; ++i) {
-                double dot_product;
+        size_t base_index;
+        size_t offset_index;
+        __m256d vec_dot_product;
+        __m256d vec1;
+        __m256d vec2;
+        alignas(32) double temp[4];
+        double dot_product;
+
+        for (size_t k = 1; k < size; ++k) {
+            for (size_t i = 0; i < size - k; ++i) {
 
                 // precompute indices
-                unsigned int base_index = index(i, i);
-                unsigned int offset_index = index(i + 1, i + k);
+                base_index = index(i, i);
+                offset_index = index(i + 1, i + k);
 
                 // Use AVX2 for SIMD operations
-                __m256d vec_dot_product = _mm256_setzero_pd();
-                // Compute dot product
-                unsigned int j = 0;
+                vec_dot_product = _mm256_setzero_pd();
+                size_t j = 0;
                 for (; j + 3 < k; j += 4) {
-                    __m256d vec1 = _mm256_loadu_pd(&data[base_index + j]);
-                    __m256d vec2 = _mm256_loadu_pd(&data[offset_index + j]);
+                    vec1 = _mm256_loadu_pd(&data[base_index + j]);
+                    vec2 = _mm256_loadu_pd(&data[offset_index + j]);
                     vec_dot_product = _mm256_fmadd_pd(vec1, vec2, vec_dot_product);
                 }
 
-                // Sum the elements of vec_dot_product
-                alignas(32) double temp[4];
                 _mm256_store_pd(temp, vec_dot_product);
                 dot_product = temp[0] + temp[1] + temp[2] + temp[3];
 
@@ -58,11 +64,12 @@ public:
                 data[base_index + k] = std::cbrt(dot_product);
             }
         }
+
     }
 
     void print() const {
-        for (unsigned int i = 0; i < size; ++i) {
-            for (unsigned int j = 0; j < size; ++j) {
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
                 if (j >= i) {
                     std::cout << std::setw(9) << std::setprecision(6) << std::fixed << data[index(i, j)] << " ";
                 } else {
@@ -75,10 +82,10 @@ public:
     }
 
 private:
-    unsigned int size;
-    alignas(32) double* data;
+    const size_t size;
+    alignas(32) double* const data;
 
-    [[nodiscard]] inline unsigned int index(unsigned int row, unsigned int column) const {
+    [[nodiscard]] inline size_t index(const size_t row, const size_t column) const {
         return (row * (2 * size - row - 1)) / 2 + column;
     }
 
