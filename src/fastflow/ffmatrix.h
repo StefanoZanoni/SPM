89d97892786d32f906471272cc3423bb42941c07
StefanoZanoni <stefano.zanoni00@gmail.com>
2024-07-26 00:13:38 +0200

    removed vectorization

--- src/fastflow/ffmatrix.h
+++ src/fastflow/ffmatrix.h
@@ -3,12 +3,10 @@
 
 #include <iostream>
 #include <cmath>
-#include <iomanip>
-#include <immintrin.h>
 #include <cstdlib>
-#include <mm_malloc.h>
 #include <ff/parallel_for.hpp>
 #include <thread>
+#include <mm_malloc.h>
 
 class FFMatrix {
 
@@ -29,36 +27,12 @@ public:
 
         for (long k = 1; k < size; ++k) {
             pf.parallel_for_static(0, size - k, 1, 0, [&](const long i) {
-
-                __m256d vec_dot_product = _mm256_setzero_pd();
-                __m256d row;
-                __m256d column;
-
-                unsigned int j = 0;
-                for (; j + 3 < k; j += 4) {
-                    row = _mm256_loadu_pd(&data[index(i, i + j)]);
-
-                    // The matrix is stored in row major order therefore the elements for the column are not contiguous
-                    int indices[4] = {
-                            static_cast<int>(index(i + j + 1, i + k)),
-                            static_cast<int>(index(i + j + 2, i + k)),
-                            static_cast<int>(index(i + j + 3, i + k)),
-                            static_cast<int>(index(i + j + 4, i + k))
-                    };
-                    column = _mm256_i32gather_pd(&data[0], _mm_loadu_si128(reinterpret_cast<const __m128i*>(indices)), 8);
-
-                    vec_dot_product = _mm256_fmadd_pd(row, column, vec_dot_product);
+                alignas(64) double dot_product = 0;
+                for (long j = 0; j < k; ++j) {
+                    dot_product += data[index(i, i + j)] * data[index(i + 1 + j, i + k)];
                 }
 
-                alignas(32) double dot_product[4];
-                _mm256_store_pd(dot_product, vec_dot_product);
-                dot_product[0] += dot_product[1] + dot_product[2] + dot_product[3];
-
-                for (; j < k; ++j) {
-                    dot_product[0] += data[index(i, i + j)] * data[index(i + 1 + j, i + k)];
-                }
-
-                data[index(i, i + k)] = std::cbrt(dot_product[0]);
+                data[index(i, i + k)] = std::cbrt(dot_product);
             });
         }
     }
--- src/sequential/matrix.h
+++ src/sequential/matrix.h
@@ -3,9 +3,6 @@
 
 #include <iostream>
 #include <cmath>
-#include <iomanip>
-#include <immintrin.h>
-#include <xmmintrin.h>
 #include <cstdlib>
 #include <mm_malloc.h>
 
@@ -14,7 +11,7 @@ class UTMatrix {
 public:
     explicit UTMatrix(const size_t size) :
     size{size},
-    data{static_cast<double*>(_mm_malloc(size * (size + 1) / 2 * sizeof(double), 32))}
+    data{static_cast<double*>(_mm_malloc(size * (size + 1) / 2 * sizeof(double), 64))}
     {
         for (size_t i = 0; i < size; ++i) {
             data[index(i, i)] = double (i + 1) / (double) size;
@@ -26,43 +23,14 @@ public:
     }
 
     void set_upper_diagonals() {
-        __m256d vec_dot_product;
-        __m256d row;
-        __m256d column;
-        alignas(32) double dot_product[4];
 
         for (size_t k = 1; k < size; ++k) {
             for (size_t i = 0; i < size - k; ++i) {
-
-                // Use AVX2 for SIMD operations
-                vec_dot_product = _mm256_setzero_pd();
-                size_t j = 0;
-                for (; j + 3 < k; j += 4) {
-                    row = _mm256_loadu_pd(&data[index(i, i + j)]);
-
-                    // The matrix is stored in row major order therefore the elements for the column are not contiguous
-                    int indices[4] = {
-                            static_cast<int>(index(i + j + 1, i + k)),
-                            static_cast<int>(index(i + j + 2, i + k)),
-                            static_cast<int>(index(i + j + 3, i + k)),
-                            static_cast<int>(index(i + j + 4, i + k))
-                    };
-                    column = _mm256_i32gather_pd(&data[0], _mm_loadu_si128(reinterpret_cast<const __m128i*>(indices)), 8);
-
-                    vec_dot_product = _mm256_fmadd_pd(row, column, vec_dot_product);
+                alignas(64) double dot_product = 0;
+                for (size_t j = 0; j < k; ++j) {
+                    dot_product += data[index(i, i + j)] * data[index(i + 1 + j, i + k)];
                 }
-
-                // Store the partial results from SIMD operations
-                _mm256_store_pd(dot_product, vec_dot_product);
-                dot_product[0] += dot_product[1] + dot_product[2] + dot_product[3];
-
-                // Handle the remaining elements
-                for (; j < k; ++j) {
-                    dot_product[0] += data[index(i, i + j)] * data[index(i + 1 + j, i + k)];
-                }
-
-                // Store the final dot product in the matrix
-                data[index(i, i + k)] = std::cbrt(dot_product[0]);
+                data[index(i, i + k)] = dot_product;
             }
         }
     }
